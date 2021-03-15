0d1afa838ddc1d309d6667f363a5e4c51adbeff2
nireo <eemil.lehtikangas1@gmail.com>
2021-03-16 01:05:44 +0200

    added some more bucket code

--- db/db.go
+++ db/db.go
@@ -4,20 +4,32 @@ package db
 
 import (
 	"errors"
+	"math"
+	"sync"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
 var (
-	ErrReadOnly  = errors.New("cannot make changes to database, since it is in read-only mode.")
-	ErrKeyLength = errors.New("the key length cannot be 0")
-	ErrNotFound  = errors.New("the key was not found")
+	// errors
+	ErrReadOnly   = errors.New("cannot make changes to database, since it is in read-only mode.")
+	ErrKeyLength  = errors.New("the key length cannot be 0")
+	ErrNotFound   = errors.New("the key was not found")
+	ErrBucketName = errors.New("the bucket name is too short")
 )
+
+// MaxBuckets is the maximum amount of buckets
+const MaxBuckets = math.MaxUint16 - (8 * 256)
 
 // DB represents the database
 type DB struct {
 	db    *leveldb.DB
 	ronly bool // indicator if the database is in the read-only mode
+
+	// buckets map maps to the identifiers, such that we can easily create a new bucket instance
+	// it is currently a map since maybe in the future I will implement a better indexing solution
+	buckets map[string][]byte
+	bmutex  sync.RWMutex
 }
 
 // Close closes the database connection
@@ -74,6 +86,50 @@ func (d *DB) DeleteNotBelonging(doesntBelong func(string) bool) error {
 	return nil
 }
 
+func (d *DB) Bucket(name string) *Bucket {
+	bucket, ok := d.bucket(name)
+	if !ok {
+		bucket, _ := d.newBucket(name)
+		return bucket
+	}
+	return bucket
+}
+
+func (d *DB) bucket(name string) (*Bucket, bool) {
+	d.bmutex.RLock()
+	bucketId, ok := d.buckets[name]
+	d.bmutex.RUnlock()
+	if !ok {
+		return nil, false
+	}
+
+	bucket := &Bucket{
+		db: d,
+		id: bucketId,
+	}
+	return bucket, true
+}
+
+func (d *DB) newBucket(name string) (*Bucket, error) {
+	if len(name) == 0 {
+		return nil, ErrBucketName
+	}
+
+	// check if a bucket already exists
+	d.bmutex.RLock()
+	if _, ok := d.buckets[name]; ok {
+		return &Bucket{db: d, id: []byte(name)}, nil
+	}
+	d.bmutex.RUnlock()
+
+	// write the bucket to the list of buckets
+	d.bmutex.Lock()
+	d.buckets[name] = []byte(name)
+	d.bmutex.Unlock()
+
+	return &Bucket{db: d, id: []byte(name)}, nil
+}
+
 // Set creates a key-value entry in the database
 func (d *DB) Set(key string, value []byte) error {
 	if d.ronly {
@@ -83,14 +139,6 @@ func (d *DB) Set(key string, value []byte) error {
 	return d.db.Put([]byte(key), value, nil)
 }
 
-// CreateBucket creates a pointer to a bucket
-func (d *DB) CreateBucket(identifier []byte) *Bucket {
-	return &Bucket{
-		db: d,
-		id: identifier,
-	}
-}
-
 // Delete removes an entry from the database
 func (d *DB) Delete(key string) error {
 	if d.ronly {
@@ -98,4 +146,12 @@ func (d *DB) Delete(key string) error {
 	}
 
 	return d.db.Delete([]byte(key), nil)
+}
+
+func prefixKey(prefix, key []byte) []byte {
+	buf := make([]byte, len(key)+len(prefix))
+	copy(buf, prefix[:])
+	copy(buf[len(prefix):], key)
+
+	return buf
 }
